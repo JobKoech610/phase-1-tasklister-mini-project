f1d1c80b3b0d2491e25722873d47fdb6a93a594d
Job koech <jobkoech610@gmail.com>
2023-03-24 10:52:31 +0300

    complete assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+
+  let form = document.getElementById("create-task-form")
+  form.addEventListener('submit',(e) =>{
+    e.preventDefault()
+
+    handleTask(e.target.new_task_description.value);
+    // handleTask(e.target.new-task-description.value)
+form.reset();
+  })
 });
+
+function handleTask(todo){
+
+  let list = document.createElement("li");
+  let btn = document.createElement("button");
+  btn.addEventListener("click", handleDelete)
+  btn.textContent= " x "
+  list.textContent=`${todo} `
+  list.appendChild(btn);
+  // console.log(list);
+  
+  document.getElementById("tasks").appendChild(list);
+  
+}
+
+function handleDelete(e){
+e.target.parentNode.remove()
+}
